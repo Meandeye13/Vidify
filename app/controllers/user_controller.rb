70c5aeffa05fae5dd4858192119eaa20fd1503a7
Ron Searl <anovusmera@gmail.com>
2012-04-21 00:01:48 -0500

    fixed up the galleria ui a bit

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,8 +3,10 @@ class UserController < ApplicationController
 
 def home
 	rootUrl = 'http://www.youtube.com/watch?v='
+	#videos2 = Video.where('src_url LIKE ?', '%youtube.com%').order("created_time DESC").limit(10) get user who posted the video (user pointer is invalidated doing projection on videos)
 	videos = Video.select(:src_url).where('src_url LIKE ?', '%youtube.com%').order("created_time DESC").limit(10)
 	@urls = []
+	@users = []
 	videos.each do |video|
 
 		url = video['src_url']
