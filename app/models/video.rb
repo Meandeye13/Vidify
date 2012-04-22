93e089dbfb1f14afaf3dbd89042ba75a6ecef50c
Frank Visaggio <fvisaggio1@gmail.com>
2012-04-22 14:27:41 -0500

    Franks change

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,10 @@
 class Video < ActiveRecord::Base
 	attr_accessible :user_id, :post_id, :created_time, :src_url, :img_src
-
+    has_many :keywords, :dependent => :destroy
 	belongs_to :user
 	validates :post_id, :uniqueness => true
-
+    
 	scope :last_update, select(:created_time).find(:first, :order => "created_time DESC")
 end
+
+
