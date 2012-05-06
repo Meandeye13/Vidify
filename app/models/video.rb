4e4071d8824199e55358d83be9facf85b27ac281
Ron Searl <anovusmera@gmail.com>
2012-05-06 16:32:16 -0500

    fixed db model error

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,8 @@
 class Video < ActiveRecord::Base
 	attr_accessible :user_id, :post_id, :created_time, :src_url, :img_src, :group_id
     has_many :keywords, :dependent => :destroy
-	belongs_to :user, :group
+	belongs_to :user
+	belongs_to :group
 	validates :post_id, :uniqueness => true
     
 	scope :last_update, select(:created_time).find(:first, :order => "created_time DESC")
